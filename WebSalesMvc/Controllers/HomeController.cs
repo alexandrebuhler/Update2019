9f97b3113a30ad2cd4869814cfb553ac0637a82f
Alexandre Buhler <al.buhler@gmail.com>
2020-05-25 11:54:05 -0300

    Inclusao de pagina About

--- WebSalesMvc/Controllers/HomeController.cs
+++ WebSalesMvc/Controllers/HomeController.cs
@@ -28,6 +28,13 @@ namespace WebSalesMvc.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Teste de mensagem...";
+            ViewData["email"] = "al.buhler@gmail.com";
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
